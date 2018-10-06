b42189a85eaac9588b926f6e068f971075025f08
zaitcev-andrey <andrey_zaycev_2000@mail.ru>
2018-10-06 16:52:05 +0300

    Масса шкафа

--- zaitcev_ar/Practice/Practice1/main.c
+++ zaitcev_ar/Practice/Practice1/main.c
@@ -4,8 +4,8 @@
 void main()
 {
 	float h, w, d, dcp, dvp, der, m1, m2, m3, m4, m5, m;
-    float tdcp, tdvp, tder;   //Толщина дсп,двп,дерева соответсвенно
-	float p;                  //Количество полок в шкафу
+    float tdcp, tdvp, tder;   //Tolshina dcp,dvp,dereva
+	float p;                  //Polki
 	setlocale(LC_CTYPE, "Rus");
 
 	dcp=750;
@@ -15,35 +15,35 @@ void main()
 	tdvp=0,5;
 	tder=1;
 
-printf(" Введите высоту(180см-220см), ширину(80см-120см) и глубину (50см-90см) шкафа");
+printf(" Vvedite visotu(180см-220см), shirinu(80см-120см) i glubinu (50см-90см) shkafa");
 scanf("%f %f %f",&h, &w, &d);
 
     if((h<180)||(h>220))
 	{
-		printf("Ошибка,некорректный ввод");
+		printf("Error,nekorrektni vvod");
 		return;
 	}
 	if((w<80)||(w>120)) 
 	{
-		printf("Ошибка,некорректный ввод"); 
+		printf("Error,nekorrektni vvod"); 
 		return;
 	}
 	if((d<50)||(d>90))
 	{
-		printf("Ошибка,некорректный ввод");
+		printf("Error,nekorrektni vvod");
 		return;
 	} 
 
 p=(h/40); 
 
-	m1 = (h * w * tdvp * dvp) / 1000000;        //Задняя стенка
-	m2 = (2 * h * d * tdcp * dcp) / 1000000;    //Боковины
-	m3 = (2 * w * d * tdcp * dcp) / 1000000;    //Верх и низ
-	m4 = (h * w * tder * der) / 1000000;        //2 двери
-	m5 = (p * (w-3) * tdcp * dcp) / 1000000;    //Полки
+	m1 = (h * w * tdvp * dvp) / 1000000;        //Zadnia stenka
+	m2 = (2 * h * d * tdcp * dcp) / 1000000;    //Bokovini
+	m3 = (2 * w * d * tdcp * dcp) / 1000000;    //Verh i niz
+	m4 = (h * w * tder * der) / 1000000;        //2 dveri
+	m5 = (p * (w-3) * tdcp * dcp) / 1000000;    //Polki
 
 	m = m1 + m2 + m3 + m4 + m5;
 
-printf("Масса шкафа(кг)=%f",m);
+printf("Macca shkafa(kg)=%f",m);
 return;
 }
