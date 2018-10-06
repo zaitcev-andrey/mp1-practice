2cbef76fd109f40e539059b7cb8e4519c2af3e49
zaitcev-andrey <andrey_zaycev_2000@mail.ru>
2018-10-06 16:34:33 +0300

    Масса шкафа

--- zaitcev_ar/Practice/Practice1/main.c
+++ zaitcev_ar/Practice/Practice1/main.c
@@ -6,6 +6,7 @@ void main()
 	float h, w, d, dcp, dvp, der, m1, m2, m3, m4, m5, m;
     float tdcp, tdvp, tder;   //Толщина дсп,двп,дерева соответсвенно
 	float p;                  //Количество полок в шкафу
+	setlocale (LC_CTYPE,"rus");
 
 	dcp=750;
 	dvp=850;
@@ -13,7 +14,6 @@ void main()
 	tdcp=1,5;
 	tdvp=0,5;
 	tder=1;
-setlocale (LC_CTYPE,"rus");
 
 printf(" Введите высоту(180см-220см), ширину(80см-120см) и глубину (50см-90см) шкафа");
 scanf("%f %f %f",&h, &w, &d);
