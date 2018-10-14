245131a4b0e5ace26beed193e488c4733172fb4a
zaitcev-andrey <andrey_zaycev_2000@mail.ru>
2018-10-14 17:01:31 +0300

    Практическое задание 0

--- zaitcev_ar/Practice/Practice0/main.c
+++ zaitcev_ar/Practice/Practice0/main.c
@@ -3,7 +3,7 @@
 void main()
 {
 	int o1 = 3, o2 = 7, r1 = 2, r2 = 3;
-	scanf_s("%d %d %d %d", &o1, &o2, &r1, &r2);
+	scanf("%d %d %d %d", &o1, &o2, &r1, &r2);
 	if((abs(o2-o1)==abs(r2-r1))||(abs(o2-o1)==abs(r2+r1)))
 	{
 		printf("one touch point");
