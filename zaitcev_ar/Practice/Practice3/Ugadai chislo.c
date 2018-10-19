8eed0ef22f143bbca667dd5ebe1f669b9dd6e17e
zaitcev-andrey <andrey_zaycev_2000@mail.ru>
2018-10-19 19:23:01 +0300

    Ugadai chislo

--- zaitcev_ar/Practice/Practice3/Ugadai chislo.c
+++ zaitcev_ar/Practice/Practice3/Ugadai chislo.c
@@ -5,21 +5,21 @@ void main ()
 {
 	int a, b, c, d;
 	char f;
+	c = 0;
 	srand ((unsigned int)time(0));
-	printf ("Vevedite 1 or 2 for dibora rejima \n");
-	scanf("%d", a);
+	printf ("Vevedite 1 ili 2 dlya vibora rejima \n");
+	scanf("%d", &a);
 
-	if ((a != 1) || (a != 2))
+	if ((a != 1) && (a != 2))
 	{
 		printf ("Neverni vvod");
-		return;
 	}
 	if (a == 1)
 	{
 		b = rand() % 1001;
 		while (1)
 		{
-			printf ("Vvedite chislo /n");
+			printf ("Vvedite chislo \n");
 			scanf ("%d", &d);
 			c++;
 			if (d == b)
@@ -28,34 +28,47 @@ void main ()
 			}
 			if (d > b)
 			{
-				printf ("Zagadannoe chislo bolshe");
+				printf ("Zagadannoe chislo bolshe \n");
 			}
 			if (d < b)
 			{
-				printf ("Zagadannoe chislo menshe");
+				printf ("Zagadannoe chislo menshe \n");
 			}
 		}
-		printf ("Vi ugadali chislo");
+		printf ("Vi ugadali chislo \n kolichestvo hodov = %d", c);
 		return;
 
 	}
 	if (a == 2)
 	{
-		
-		printf ("Zagadai chislo");
-		b = rand() % 1001
+		printf ("Zagadai chislo.");
+		b = rand() % 1001;
 	    while (1)
 		{
 			int b2;
 			c++;
-			printf ("Kak programma reagiruet na chislo b(ot 0 do 1000) %d \n Vvedite znak otnosheniya( >, <, =)", b);
-			scanf ("%c",f);
+			printf ("Kak programma reagiruet na chislo b(ot 0 do 1000) %d \n Vvedite znak otnosheniya( >, <, =) \n", b);
+			scanf ("%c", &f);
 			switch (f)
 			{
-			case '=' : 
-			}
-			
+			case '=':
+				break;
+			case '>':
+				while (b > b2)
+				{
+					b2 = rand() % 1001; 
+				}
+				b = b2;
+			case '<':
+				while (b < b2)
+				{
+					b2 = rand() % 1001;
+				}
+				b = b2;
+			default: printf ("Neverni vvod.");
+			}			
 		}
+		printf ("Vashe chislo %d \n kolichestvo hodov = %d", b, c);  
+		return;
 	}
-
 }
