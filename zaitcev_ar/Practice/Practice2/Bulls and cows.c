fcfd7fe974427f3b8c5b4b5e506db0f728da45af
unknown <zajtsev.a.r@WS-6K-102-A13.ccam.cmc.unn.net>
2018-10-22 15:30:50 +0300

    Add 2

--- zaitcev_ar/Practice/Practice2/Bulls and cows.c
+++ zaitcev_ar/Practice/Practice2/Bulls and cows.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#define N 10
 void main ()
 {
+	int n;
 	srand((unsigned int)time(0));
+	do
+	{
+		printf("Vvedite kolichestvo tcifr v zagadannom chisle (do 10) \n");
+		scanf("%d", &n);
+	}while ((n <= 0)||(n > N));
 }
