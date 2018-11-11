7c378c860d4b6a31dae8c7ec3851449784662b80
zaitcev-andrey <andrey_zaycev_2000@mail.ru>
2018-11-11 21:19:25 +0300

    Fix mass of wardrobe

--- zaitcev_ar/Practice/Practice1/main.c
+++ zaitcev_ar/Practice/Practice1/main.c
@@ -4,44 +4,41 @@ void main()
 {
 	float h, w, d, dcp, dvp, der, m1, m2, m3, m4, m5, m;
     float tdcp, tdvp, tder;   //Tolshina dcp,dvp,dereva
-	float p;                  //Polki
 
-	dcp=750;
-	dvp=850;
-	der=720;
-	tdcp=1,5;
-	tdvp=0,5;
-	tder=1;
+	tdcp = 0.015;
+	tdvp = 0.005;
+	tder = 0.01;
 
-printf(" Vvedite visotu(180cm-220cm), shirinu(80cm-120cm) i glubinu (50cm-90cm) shkafa");
-scanf("%f %f %f",&h, &w, &d);
+	printf(" Vvedite plotnost dcp(700-800), dvp(800-900), der(670-770) shkafa \n ");
+	scanf("%f %f %f", &dcp, &dvp, &der);
 
-    if((h<180)||(h>220))
+    printf(" Vvedite visotu(1.8m-2.2m), shirinu(0.8m-1.2m) i glubinu (0.5m-0.9m) shkafa \n ");
+    scanf("%f %f %f",&h, &w, &d);
+
+    if((h < 1.8) || (h > 2.2))
 	{
 		printf("Error,nekorrektni vvod");
 		return;
 	}
-	if((w<80)||(w>120)) 
+	if((w < 0.8) || (w > 1.2)) 
 	{
 		printf("Error,nekorrektni vvod"); 
 		return;
 	} 
-	if((d<50)||(d>90))
+	if((d < 0.5) || (d > 0.9))
 	{
 		printf("Error,nekorrektni vvod");
 		return;
 	} 
 
-       p = (h/40); 
-
-	m1 = (h * w * tdvp * dvp) / 1000000;        //Zadnia stenka
-	m2 = (2 * h * d * tdcp * dcp) / 1000000;    //Bokovini
-	m3 = (2 * w * d * tdcp * dcp) / 1000000;    //Verh i niz
-	m4 = (h * w * tder * der) / 1000000;        //2 dveri
-	m5 = (p * (w-3) * tdcp * dcp) / 1000000;    //Polki
+	m1 = (h * w * tdvp * dvp);                                                       //Zadnia stenka
+	m2 = (2 * h * d * tdcp * dcp);                                                   //Bokovini
+	m3 = (2 * w * d * tdcp * dcp);                                                   //Verh i niz
+	m4 = (h * w * tder * der);                                                       //2 dveri
+	m5 = ((h - 2 * 0.015) / (0.4 + 0.015) * (w - 2 * 0.015) * (d - 0.005) * dcp);    //Polki
 
 	m = m1 + m2 + m3 + m4 + m5;
 
-printf("Macca shkafa(kg)=%f",m);
-return;
+    printf(" Macca shkafa(kg)=%f ", m);
+    return;
 }
