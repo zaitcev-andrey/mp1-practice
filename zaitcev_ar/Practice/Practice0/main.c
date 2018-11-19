d41c6d216b51a32cb16556ed7d908254db9bf25b
zaitcev-andrey <andrey_zaycev_2000@mail.ru>
2018-11-19 09:36:54 +0300

    Fix 0

--- zaitcev_ar/Practice/Practice0/main.c
+++ zaitcev_ar/Practice/Practice0/main.c
@@ -1,20 +1,22 @@
 #include <math.h>
 #include <stdio.h>
+
 void main()
 {
-	int o1 = 3, o2 = 7, r1 = 2, r2 = 3;
+	int o1, o2, r1, r2;
+	printf("Vvedite o1, o2, r1, r2 \n");
 	scanf("%d %d %d %d", &o1, &o2, &r1, &r2);
-	if((abs(o2-o1)==abs(r2-r1))||(abs(o2-o1)==abs(r2+r1)))
+	if ((abs(o2 - o1) == abs(r2 - r1)) || (abs(o2 - o1) == abs(r2 + r1)))
 	{
 		printf("one touch point");
 		return;
 	}
-	if((abs(o2-o1)<abs(r2-r1))||(abs(o2-o1)>abs(r2+r1)))
+	if ((abs(o2 - o1) < abs(r2 - r1)) || (abs(o2 - o1) > abs(r2 + r1)))
 	{
 		printf("do not intersect");
 		return;
 	}
-	if(abs(r2-r1)<abs(o2-o1)<abs(r2+r1))
+	if (abs(r2 - r1) < abs(o2 - o1) < abs(r2 + r1))
 	{
 		printf("intersect");
 	}	
