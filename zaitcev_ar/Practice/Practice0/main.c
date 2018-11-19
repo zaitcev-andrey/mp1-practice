fcaa06008b2cfecaacf66c0d94cb757a8c85d55b
zaitcev-andrey <andrey-zaycev_2000@mail.ru>
2018-11-19 15:46:14 +0300

    Fix-0

--- zaitcev_ar/Practice/Practice0/main.c
+++ zaitcev_ar/Practice/Practice0/main.c
@@ -3,21 +3,51 @@
 
 void main()
 {
-	int o1, o2, r1, r2;
-	printf("Vvedite o1, o2, r1, r2 (centri i radiusi 1 i 2 okrujnosti) \n");
-	scanf("%d %d %d %d", &o1, &o2, &r1, &r2);
-	if ((abs(o2 - o1) == abs(r2 - r1)) || (abs(o2 - o1) == abs(r2 + r1)))
-	{
-		printf("one touch point");
-		return;
-	}
-	if ((abs(o2 - o1) < abs(r2 - r1)) || (abs(o2 - o1) > abs(r2 + r1)))
-	{
-		printf("do not intersect");
-		return;
-	}
-	if (abs(r2 - r1) < abs(o2 - o1) < abs(r2 + r1))
-	{
-		printf("intersect");
-	}	
+    double r1, r2, x1, x2, y1, y2;
+    double R;   //Radius mejdu centrami okrujnostey. Vichislyaem ego po T.Pifagora
+    printf("Vvedite: r1 (r1 > 0), r2 (r2 > 0), x1, x2, y1, y2\n");
+    scanf("%lf %lf %lf %lf %lf %lf", &r1, &r2, &x1, &x2, &y1, &y2);
+    R = sqrt(((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
+
+    if ((r1 <= 0) || (r2 <= 0))
+    {
+        printf("Neverni vvod(okrujnost(i) ne sushestvuyt)\n");
+        return;
+    }
+
+    if (R > (r1 + r2))
+    {
+        printf("Ne imeut obshih tochek\n");
+        return;
+    }
+
+    if (R == (r1 + r2))
+    {
+        printf("Okrujnosti kasautcya, odna okr. ne vnutri drugoy\n");
+        return;
+    }
+
+    if (R == fabs(r1 - r2))
+    {
+        printf("Okrujnosti kasautcya, odna okr. vnutri drugoy\n");
+        return;
+    }
+
+    if (R < fabs(r1 - r2))
+    {
+        printf("Net obshih tochek, odna okr. vnutri drugoy\n");
+        return;
+    }
+
+    if ((r1 == r2) && (x1 == x2) && (y1 == y2))
+    {
+        printf("Okrujnosti sovpadaut, y nih beskonechno mnogo obshih tochek\n");
+        return;
+    }
+
+    else
+    {
+        printf("Peresecautcya v 2-uh obshih tochkah");
+        return;
+    }
 }
