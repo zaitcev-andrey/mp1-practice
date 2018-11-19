7ecd8c8963696430beec2e7414a15c8fd03f7019
zaitcev-andrey <andrey_zaycev_2000@mail.ru>
2018-11-19 09:40:48 +0300

    Fix 0

--- zaitcev_ar/Practice/Practice0/main.c
+++ zaitcev_ar/Practice/Practice0/main.c
@@ -4,7 +4,7 @@
 void main()
 {
 	int o1, o2, r1, r2;
-	printf("Vvedite o1, o2, r1, r2 \n");
+	printf("Vvedite o1, o2, r1, r2 (centri i radiusi 1 i 2 okrujnosti) \n");
 	scanf("%d %d %d %d", &o1, &o2, &r1, &r2);
 	if ((abs(o2 - o1) == abs(r2 - r1)) || (abs(o2 - o1) == abs(r2 + r1)))
 	{
